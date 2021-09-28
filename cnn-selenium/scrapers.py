9fa1012df36b739a2cafa891d36c95ffe0e2c48d
RuslanUchan <ruslanuchan97@gmail.com>
2021-09-28 15:36:33 +0700

    selenium get href working

--- cnn-selenium/scrapers.py
+++ cnn-selenium/scrapers.py
@@ -1,12 +1,27 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+URL = 'https://www.cnnindonesia.com/indeks'
 
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver.get(URL)
+
+# https://stackoverflow.com/a/65142989/8996974
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='date1']"))).click()
+print('interacted with datepicker')
+
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='datepick-month-header']/select[@title='Change the month']/option[@value='5/2021']"))).click()
+print('interacted with monthpicker')
+
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='datepick-month']/table/tbody//a[text()='1']"))).click()
+print('interacted with daypicker')
+
+driver.implicitly_wait(5)
+articles = driver.find_elements_by_xpath("//a[contains(@href, '20210501')]")
+
+for article in articles:
+	print(article.get_attribute('href'))
+
 driver.close()
